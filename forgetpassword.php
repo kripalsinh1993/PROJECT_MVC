54441f954a07e2273cb90e93844e00c09bf1fe0d
kripalsinh1993 <kripalsinhraa043@gmmail.com>
2023-03-07 22:11:35 +0530

    data is successfully

---
+++ forgetpassword.php
@@ -0,0 +1,26 @@
+<!-- content -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 mt-5">
+
+            <h2 class="mt-3" align="center">Forgot Password ?&nbsp;<i class="bi bi-arrow-right-square"></i></h2>
+            <hr class="border border-1 border-dark w-25 mx-auto">
+
+            <div class="card mt-3 w-50 mx-auto">
+                <div class="card-header bg-primary"></div>
+                <div class="card-body">
+                    <form method="post">
+                        <div class="form-group  mt-3">
+                            <input type="text" name="email" placeholder="Email *" required class="form-control">
+                        </div>
+
+                        <div class="form-group mt-3">
+                            <input type="submit" name="fpass" value="Submit" class="btn btn-lg bg-info" id="btn">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- //content -->
